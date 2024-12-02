c3e14e2f9202e6a0098651508f41e6cbb24ab143
Ahmad Basyir <abasyir@sheerscomputer.com>
2024-12-02 15:46:41 +0800

    Initial commit

---
+++ LogSearch/LogSearch.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LogSearchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TitleLabel = New Label()
+        SearchBtn = New Button()
+        FilePath = New TextBox()
+        FilePathLabel = New Label()
+        SearchBoxLabel = New Label()
+        SearchBox = New TextBox()
+        txtOutput = New TextBox()
+        logOutput = New TextBox()
+        txtOutputLabel = New Label()
+        logOutputLabel = New Label()
+        ExportBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' TitleLabel
+        ' 
+        TitleLabel.AutoSize = True
+        TitleLabel.Location = New Point(334, 20)
+        TitleLabel.Name = "TitleLabel"
+        TitleLabel.Size = New Size(101, 21)
+        TitleLabel.TabIndex = 0
+        TitleLabel.Text = "Log Searcher"
+        ' 
+        ' SearchBtn
+        ' 
+        SearchBtn.Location = New Point(633, 81)
+        SearchBtn.Name = "SearchBtn"
+        SearchBtn.Size = New Size(116, 74)
+        SearchBtn.TabIndex = 1
+        SearchBtn.Text = "Search"
+        SearchBtn.UseVisualStyleBackColor = True
+        ' 
+        ' FilePath
+        ' 
+        FilePath.Location = New Point(153, 83)
+        FilePath.Name = "FilePath"
+        FilePath.Size = New Size(440, 29)
+        FilePath.TabIndex = 2
+        ' 
+        ' FilePathLabel
+        ' 
+        FilePathLabel.AutoSize = True
+        FilePathLabel.Location = New Point(26, 86)
+        FilePathLabel.Name = "FilePathLabel"
+        FilePathLabel.Size = New Size(70, 21)
+        FilePathLabel.TabIndex = 3
+        FilePathLabel.Text = "Log Path"
+        ' 
+        ' SearchBoxLabel
+        ' 
+        SearchBoxLabel.AutoSize = True
+        SearchBoxLabel.Location = New Point(26, 134)
+        SearchBoxLabel.Name = "SearchBoxLabel"
+        SearchBoxLabel.Size = New Size(71, 21)
+        SearchBoxLabel.TabIndex = 4
+        SearchBoxLabel.Text = "Keyword"
+        ' 
+        ' SearchBox
+        ' 
+        SearchBox.Location = New Point(153, 131)
+        SearchBox.Name = "SearchBox"
+        SearchBox.Size = New Size(216, 29)
+        SearchBox.TabIndex = 5
+        ' 
+        ' txtOutput
+        ' 
+        txtOutput.Location = New Point(26, 246)
+        txtOutput.Multiline = True
+        txtOutput.Name = "txtOutput"
+        txtOutput.Size = New Size(343, 160)
+        txtOutput.TabIndex = 6
+        ' 
+        ' logOutput
+        ' 
+        logOutput.Location = New Point(406, 246)
+        logOutput.Multiline = True
+        logOutput.Name = "logOutput"
+        logOutput.Size = New Size(343, 160)
+        logOutput.TabIndex = 7
+        ' 
+        ' txtOutputLabel
+        ' 
+        txtOutputLabel.AutoSize = True
+        txtOutputLabel.Location = New Point(26, 213)
+        txtOutputLabel.Name = "txtOutputLabel"
+        txtOutputLabel.Size = New Size(83, 21)
+        txtOutputLabel.TabIndex = 8
+        txtOutputLabel.Text = ".txt Output"
+        ' 
+        ' logOutputLabel
+        ' 
+        logOutputLabel.AutoSize = True
+        logOutputLabel.Location = New Point(406, 213)
+        logOutputLabel.Name = "logOutputLabel"
+        logOutputLabel.Size = New Size(88, 21)
+        logOutputLabel.TabIndex = 9
+        logOutputLabel.Text = ".log Output"
+        ' 
+        ' ExportBtn
+        ' 
+        ExportBtn.Location = New Point(633, 412)
+        ExportBtn.Name = "ExportBtn"
+        ExportBtn.Size = New Size(116, 36)
+        ExportBtn.TabIndex = 10
+        ExportBtn.Text = "Export"
+        ExportBtn.UseVisualStyleBackColor = True
+        ' 
+        ' LogSearchForm
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 21F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(783, 470)
+        Controls.Add(ExportBtn)
+        Controls.Add(logOutputLabel)
+        Controls.Add(txtOutputLabel)
+        Controls.Add(logOutput)
+        Controls.Add(txtOutput)
+        Controls.Add(SearchBox)
+        Controls.Add(SearchBoxLabel)
+        Controls.Add(FilePathLabel)
+        Controls.Add(FilePath)
+        Controls.Add(SearchBtn)
+        Controls.Add(TitleLabel)
+        Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ForeColor = Color.Black
+        Margin = New Padding(4)
+        Name = "LogSearchForm"
+        Text = "Log Searcher"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TitleLabel As Label
+    Friend WithEvents SearchBtn As Button
+    Friend WithEvents FilePath As TextBox
+    Friend WithEvents FilePathLabel As Label
+    Friend WithEvents SearchBoxLabel As Label
+    Friend WithEvents SearchBox As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents logOutput As TextBox
+    Friend WithEvents txtOutputLabel As Label
+    Friend WithEvents logOutputLabel As Label
+    Friend WithEvents ExportBtn As Button
+End Class
---
+++ LogSearch/LogSearch.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Buffers
+Imports System.IO
+
+Public Class LogSearchForm
+    Private Sub SearchBtn_Click(sender As Object, e As EventArgs) Handles SearchBtn.Click
+        ' Get the log folder path and keyword input from the user
+        Dim logDirectory As String = FilePath.Text
+        Dim keyword As String = SearchBox.Text
+
+        ' Check if the log folder exists
+        If Not System.IO.Directory.Exists(logDirectory) Then
+            MessageBox.Show("Log folder does not exist.")
+            Return
+        End If
+
+        ' Search for files containing the keyword
+        Dim txtFiles As New List(Of String)
+        Dim logFiles As New List(Of String)
+
+        ' Get all text files in the folder
+        Dim files() As String = System.IO.Directory.GetFiles(logDirectory)
+
+        ' Iterate through each file and search for the keyword
+        For Each file As String In files
+            Dim extension As String = System.IO.Path.GetExtension(file).ToLower()
+            If extension = ".txt" OrElse extension = ".log" Then
+                Dim lines() As String = System.IO.File.ReadAllLines(file)
+                For Each line As String In lines
+                    If line.Contains(keyword) Then
+                        If extension = ".txt" Then
+                            txtFiles.Add(file)
+                        ElseIf extension = ".log" Then
+                            logFiles.Add(file)
+                        End If
+                        Exit For ' Once found in this file, no need to continue searching
+                    End If
+                Next
+            End If
+        Next
+
+        ' Display the matching files in the text boxes
+        DisplayFiles(txtFiles, txtOutput)
+        DisplayFiles(logFiles, logOutput)
+
+    End Sub
+
+    Private Sub DisplayFiles(files As List(Of String), textBox As TextBox)
+        ' Display the matching files in the text box
+        Dim output As String = ""
+        For Each file As String In files
+            output += file & Environment.NewLine
+        Next
+        textBox.Text = output
+    End Sub
+
+    Private Sub ExportToFile(files As List(Of String), filePath As String)
+        Try
+            ' Write the file paths to the text file
+            Using writer As New StreamWriter(filePath)
+                For Each file As String In files
+                    writer.WriteLine(file)
+                Next
+            End Using
+            MessageBox.Show("File paths exported successfully.", "Export Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error exporting file paths: " & ex.Message, "Export Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ExportBtn_Click(sender As Object, e As EventArgs) Handles ExportBtn.Click
+        ' Create a SaveFileDialog instance
+        Dim saveFileDialog As New SaveFileDialog()
+
+        ' Set properties for SaveFileDialog
+        saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+        saveFileDialog.Title = "Export File Paths"
+        saveFileDialog.FileName = "ExportedFilePaths.txt"
+        saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+
+        ' Show the SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Get the file paths from the text boxes
+            Dim txtFilePaths As New List(Of String)(txtOutput.Lines)
+            Dim logFilePaths As New List(Of String)(logOutput.Lines)
+
+            ' Export the file paths to the selected file
+            ExportToFile(txtFilePaths, saveFileDialog.FileName)
+            ExportToFile(logFilePaths, saveFileDialog.FileName)
+        End If
+    End Sub
+End Class
